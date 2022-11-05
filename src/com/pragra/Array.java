ff7fd0631d5571ff0f2530b59b565e6621e3b338
andyysi <arnejaanand@gmail.com>
2022-11-05 11:36:10 -0400

    newfile

--- src/com/pragra/Array.java
+++ src/com/pragra/Array.java
@@ -1,5 +1,23 @@
 package com.pragra;
-
+import java.util.Scanner;
 public class Array {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter your marks");
+        int sum = 0;
+        for(int i= 0; i<=5; i++ ){
+        int marks = sc.nextInt();
+        System.out.println();
+
+
+
+
+
+
+
+    }
+
+
+
 
 }
