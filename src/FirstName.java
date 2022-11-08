23ad71d32bdcbfdd8b35a0d7f2a4c99b16171ddf
MRizwan7 <117256951+MRizwan7@users.noreply.github.com>
2022-11-07 20:39:49 -0500

    Update FirstName.java

--- src/FirstName.java
+++ src/FirstName.java
@@ -1,5 +1,6 @@
 public class FirstName {
         public static void main(String[] names) {
             System.out.println(names[0]);
+                Systrm.out.println("Hi")
         }
             }
